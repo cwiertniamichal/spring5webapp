9efb4a53c2aa4e86645d87fe4a4279507ebff69c
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-06-27 22:41:40 +0200

    Added template. Fixed toString error.

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -87,8 +87,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
